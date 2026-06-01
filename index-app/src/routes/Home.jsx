6199fa040488e006b55b28ce8a81870a0c113d6d
Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
2026-06-01 14:40:55 +0200

    feature: setting up home route

---
+++ index-app/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="min-h-screen">
+            <div className="p4">
+                Hallo!
+            </div>
+        </div>
+    )
+}
+
+export default Home
