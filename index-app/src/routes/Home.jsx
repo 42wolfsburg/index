ebb7fc9dfaaeab466ddca56292f45e155b3d5e2f
Felipe Dessoy Caraballo <felipe@42wolfsburg.de>
2026-06-01 15:00:39 +0200

    feature: structure for website

--- index-app/src/routes/Home.jsx
+++ index-app/src/routes/Home.jsx
@@ -1,9 +1,18 @@
+import { Header, Footer, Card } from '../components'
+
+const items = [
+    {title: "1", description: "description"},
+    {title: "2", description: "description"},
+    {title: "3", description: "description"},
+]
+
 const Home = () => {
     return (
-        <div className="min-h-screen">
-            <div className="p4">
-                Hallo!
-            </div>
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 p-8 grid grid-cols-1 lg:grid-cols-3 gap-6">
+                {items.map((item, i) => <Card key={i} {...item} />)}
+            </main>
         </div>
     )
 }
